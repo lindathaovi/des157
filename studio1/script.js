4c41d21ffa4abbebe549051c9561896b174e7f1d
Linda Tran <lindathaovi@Lindas-MacBook-Pro.local>
2017-01-16 23:00:28 -0800

    adding banner

---
+++ studio1/script.js
@@ -0,0 +1,11 @@
+function setup() {
+    var myCanvas = createCanvas(800, 250);
+    //parent the myCanvas variable to the html element titled “mySketch”
+    myCanvas.parent('mySketch');
+
+}
+
+function draw() {
+    background(0);
+
+}
