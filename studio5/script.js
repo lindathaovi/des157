134ab286ea5ee6acd084de8f2e9a1dffc290dfb3
Linda Tran <lindathaovi@campus-101-079.ucdavis.edu>
2017-02-21 12:37:21 -0800

    studio 5

---
+++ studio5/script.js
@@ -0,0 +1,50 @@
+// JavaScript Document
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded and parsed");
+
+    // all other js here
+
+    //assemble api url
+    // var originalMsg = document.getElementById('originalMsg').innerHTML; //returns text in original msg
+    var key="AIzaSyD3Q0DjTctqKY9vZCuPdgKmgnIrDTXlvow";
+    var source="en";
+    var target="fr";
+    var url = 'https://www.googleapis.com/language/translate/v2?';
+
+    //capture submit event
+    document.chatbox.onsubmit = processForm(url, key, source, target);
+
+
+//define processform
+function processForm (url, key, source, target) {
+   //store message into variable
+  //  var originalMsg = document.chatbox.originalMsg.value;
+
+   var translatedMsg = document.getElementById('translatedMsg'); //capture translated element
+  //  translatedMsg.innerHTML= 'Your message translated: '+ trans; //call innerhtml to place new translated message
+
+  var originalMsg = document.chatbox.originalMsg.value;
+  url += 'key='+key+'&source='+source+'&target='+target+'&q='+originalMsg;
+
+  // translate text
+  translate(url);
+
+  return false; //prevent page from reloading
+}
+
+//parse json into js object
+function translate(url) {
+      console.log(url)
+      var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          myObj = JSON.parse(this.responseText);
+          document.getElementById("translatedMsg").innerHTML = myObj.data.translations[0].translatedText;
+      }
+  };
+  xmlhttp.open("GET", url , true);
+  xmlhttp.send();
+}
+
+});
