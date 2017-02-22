a1858d71778eb52e7de9d3c0ca217825eff36a35
Linda Tran <lindathaovi@campus-101-079.ucdavis.edu>
2017-02-21 16:00:49 -0800

    clean up

--- studio5/script.js
+++ studio5/script.js
@@ -3,30 +3,22 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
-    // all other js here
-
     //assemble api url
-    // var originalMsg = document.getElementById('originalMsg').innerHTML; //returns text in original msg
-    var key="AIzaSyD3Q0DjTctqKY9vZCuPdgKmgnIrDTXlvow";
-    var source="en";
-    var target="fr";
+    var key = "AIzaSyD3Q0DjTctqKY9vZCuPdgKmgnIrDTXlvow";
+    var source = "en";
+    var target = "fr";
     var url = 'https://www.googleapis.com/language/translate/v2?';
 
     //capture submit event
-    //document.chatbox.onsubmit = processForm(url, key, source, target);
+    document.chatbox.onsubmit = processForm2;
 
-    document.chatbox.onsubmit=processForm2;
-    function processForm2(){
-      url = 'https://www.googleapis.com/language/translate/v2?';
-      console.log('processForm2');
-      //store message into variable
-     //   //  var originalMsg = document.chatbox.originalMsg.value;
+    function processForm2() {
+        url = 'https://www.googleapis.com/language/translate/v2?'; //resets api url when resubmitted
+        console.log('processForm2');
 
-       var translatedMsg = document.getElementById('translatedMsg'); //capture translated element
-     //  translatedMsg.innerHTML= 'Your message translated: '+ trans; //call innerhtml to place new translated message
-
-      var originalMsg = document.chatbox.originalMsg.value;
-        url += 'key='+key+'&source='+source+'&target='+target+'&q='+originalMsg;
+        var translatedMsg = document.getElementById('translatedMsg'); //capture translated element
+        var originalMsg = document.chatbox.originalMsg.value;
+        url += 'key=' + key + '&source=' + source + '&target=' + target + '&q=' + originalMsg;
 
         // translate text
         translate(url);
@@ -34,37 +26,34 @@ document.addEventListener("DOMContentLoaded", function() {
         return false; //prevent page from reloading
     }
 
+    // //define processform
+    // function processForm(url, key, source, target) {
+    //     console.log('processForm');
+    //
+    //     var translatedMsg = document.getElementById('translatedMsg'); //capture translated element
+    //
+    //     var originalMsg = document.chatbox.originalMsg.value;
+    //     url += 'key=' + key + '&source=' + source + '&target=' + target + '&q=' + originalMsg;
+    //
+    //     // translate text
+    //     translate(url);
+    //
+    //     return false; //prevent page from reloading
+    // }
 
-//
-// //define processform
-// function processForm (url, key, source, target) {
-//   console.log('processForm');
-//    //store message into variable
-//   //  var originalMsg = document.chatbox.originalMsg.value;
-//
-//    var translatedMsg = document.getElementById('translatedMsg'); //capture translated element
-//   //  translatedMsg.innerHTML= 'Your message translated: '+ trans; //call innerhtml to place new translated message
-//
-//   var originalMsg = document.chatbox.originalMsg.value;
-//   url += 'key='+key+'&source='+source+'&target='+target+'&q='+originalMsg;
-//
-//   // translate text
-//   translate(url);
-//
-//   return false; //prevent page from reloading
-// }
-//parse json into js object
-function translate(url) {
-      console.log(url)
-      var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-          myObj = JSON.parse(this.responseText);
-          document.getElementById("translatedMsg").innerHTML = myObj.data.translations[0].translatedText;
-      }
-  };
-  xmlhttp.open("GET", url , true);
-  xmlhttp.send();
-}
+
+    //parse json into js object
+    function translate(url) {
+        console.log(url)
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                myObj = JSON.parse(this.responseText);
+                document.getElementById("translatedMsg").innerHTML = myObj.data.translations[0].translatedText;
+            }
+        };
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    }
 
 });
