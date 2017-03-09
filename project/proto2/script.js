f173a985854c2876503dcfef1aeefa4da947e76f
Linda Tran <lindathaovi@Lindas-MacBook-Pro.local>
2017-03-09 04:16:51 -0800

    proto2

---
+++ project/proto2/script.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded and parsed");
+
+    //LOGIN BUTTON
+
+    document.getElementById("loginbtn").onclick = function() {
+        location.href = 'index.html';
+    };
+
+
+});
