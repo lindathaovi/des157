bcfbf9909cc37b26ac7c5fdeb7ab1f6351531920
Linda Tran <lindathaovi@campus-109-023.ucdavis.edu>
2017-03-09 14:51:32 -0800

    js edit

--- project/proto2/script.js
+++ project/proto2/script.js
@@ -4,10 +4,14 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
     //LOGIN BUTTON
+    // document.getElementById("loginbtn").onclick = function() {
+    //     location.href = 'index.html';
+    // };
 
-    document.getElementById("loginbtn").onclick = function() {
-        location.href = 'index.html';
-    };
+
+
+
+    
 
 
 });
