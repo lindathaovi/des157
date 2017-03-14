b7c4adf41d92f37d682fc9f68ae4c93f3b4bd7a6
Linda Tran <lindathaovi@campus-002-064.ucdavis.edu>
2017-03-14 13:09:44 -0700

    pop up

--- project/proto3/scripts/script.js
+++ project/proto3/scripts/script.js
@@ -3,33 +3,42 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
-window.onload = msgprompt;
-
-var prompt = document.getElementById('promptpop');
-var close = document.getElementById('close');
-
-function msgprompt(){
-  //display popup window for todays prompt
-  prompt.style.display='block';
-
-  close.addEventListener('click', function () {
-        prompt.style.display = 'none'; });
-
-}
+    window.onload = msgprompt;
+    window.onload = usertesting;
 
 
+    var prompt = document.getElementById('promptpop');
+    var loginpop = document.getElementById('loginpop');
+    var logoutpop = document.getElementById('logoutpop');
+    var clicklogout= document.getElementById('clicklogout');
+    var logoutverify= document.getElementById('logoutbtn');
+    var close = document.getElementById('close');
+
+    function msgprompt() {
+        //display popup window for todays prompt
+        prompt.style.display = 'block';
 
 
+        close.addEventListener('click', function() {
+            prompt.style.display = 'none';
+        });
+    }
+
+    function usertesting() {
+        loginpop.style.display = 'block';
+        close.addEventListener('click', function() {
+            loginpop.style.display = 'none';
+        });
+
+    }
 
 
+        logoutbutton.addEventListener('click', function() {
+        logoutpop.style.display = 'block'; });
 
-
-
-
-
-
-
-
+        close.addEventListener('click', function() {
+            logoutpop.style.display = 'none';
+        });
 
 
 
