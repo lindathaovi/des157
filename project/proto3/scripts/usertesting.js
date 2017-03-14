09918cd69977f31ce99ac7afafa41a765503c942
Linda Tran <lindathaovi@campus-002-064.ucdavis.edu>
2017-03-14 12:38:28 -0700

    pop up message box

--- project/proto3/scripts/usertesting.js
+++ project/proto3/scripts/usertesting.js
@@ -3,14 +3,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
-    window.alert("Hello There! Welcome to SocialCue. Thank you for testing out this app! Some features are still being worked out including the messenger!");
-
-
-
-
-
-
-
+    window.alert("Hello There! Welcome to SocialCue. Thank you for testing out this app! Please be aware that the app is not fully functional. Some features are still being worked out including the messenger!");
 
 
 });
