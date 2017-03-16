b0f56be5e26d5c8353cd1bdd4d41579e174cac68
Linda Tran <lindathaovi@Lindas-MacBook-Pro.local>
2017-03-15 17:40:20 -0700

    updates

--- project/final/scripts/messenger.js
+++ project/final/scripts/messenger.js
@@ -3,7 +3,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
-    window.alert("Hello There! Welcome to SocialCue. Thank you for testing out this app! Some features are still being worked out including the messenger!");
+
 
     'use strict';
 
